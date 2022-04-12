37d0b82e5291614312d1ec90a8dcacfd68e14ce5
Jan "yenda" Trmal <jtrmal@gmail.com>
2022-04-12 12:58:16 -0400

    tiny batch

--- egs/cmu_cslu_kids/s5/local/sum_duration.py
+++ egs/cmu_cslu_kids/s5/local/sum_duration.py
@@ -1,15 +1,17 @@
-# Sum duration obtained by using 
+#!/usr/bin/env python3
+# Sum duration obtained by using
 # utils/data/get_utt2dur.sh
 
 import sys
+
 file = sys.argv[1]
 sum = 0
-with open(file, 'r') as fp:
+with open(file, "r") as fp:
     line = fp.readline()
-    while(line):
+    while line:
         toks = line.strip().split()
         sum += float(toks[1])
         line = fp.readline()
 fp.close()
-h=sum/3600
-sys.stdout.write("%f hour data.\n"%h)
+h = sum / 3600
+sys.stdout.write("%f hour data.\n" % h)
